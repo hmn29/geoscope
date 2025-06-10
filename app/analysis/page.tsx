946426d12425b7669926e4b071cef2ca37fb5faf
Harman <hmn30004@gmail.com>
2025-06-10 22:17:31 +0530

    Updated page.tsx

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -1085,7 +1085,7 @@ export default function AnalysisPage() {
                   </div>
                 </div>
               </CardHeader>
-              <CardContent className="p-4 h-[790px]">
+              <CardContent className="p-4 h-[770px]">
                 <MapView
                   coordinates={coordinates}
                   selectedLocation={selectedLocation}
